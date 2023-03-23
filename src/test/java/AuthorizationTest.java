7c92198546a66e39e882e88981fee52f440161e0
iampolshin <i.am.polshin@gmail.com>
2023-03-23 21:23:31 +0300

    Задание на паре

--- src/test/java/AuthorizationTest.java
+++ src/test/java/AuthorizationTest.java
@@ -1,28 +1,45 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import pages.AuthorizationPage;
+import pages.MainPage;
 
 import java.io.InputStream;
 import java.util.Properties;
 
+import static matcher.IsPositiveInteger.isAPositiveInteger;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 import static org.junit.Assert.fail;
 
 public class AuthorizationTest {
     private static final InputStream input = AuthorizationTest.class.getClassLoader()
             .getResourceAsStream("config.properties");
-    private static final AuthorizationPage authorizationPage = new AuthorizationPage();
-    private static final Properties prop = new Properties();
+    private static final AuthorizationPage AUTHORIZATION_PAGE = new AuthorizationPage();
+    private static final MainPage MAIN_PAGE = new MainPage();
+    private static final Properties PROP = new Properties();
 
-    @Test
+    @Before
     public void testLogin() {
         try {
-            prop.load(input);
+            PROP.load(input);
         } catch (Exception e) {
             fail("Не удалось получить доступ к ресурсам.");
         }
-        String login = prop.getProperty("login");
-        String pass = prop.getProperty("pass");
-        Assert.assertTrue(authorizationPage.login(login, pass)
-                .isAuthorized());
+        String login = PROP.getProperty("login");
+        String pass = PROP.getProperty("pass");
+        Assert.assertTrue(AUTHORIZATION_PAGE.login(login, pass).isAuthorized());
+    }
+
+    @Test
+    public void hasMessages() {
+        assertThat(MAIN_PAGE.getMessageCount(), isAPositiveInteger());
+    }
+
+    // Проверяет, есть ли на странице рекомендация "Идеи дня"
+    @Test
+    public void hasIdeas() {
+        String text = "Идеи дня";
+        assertThat(MAIN_PAGE.getIdeas(), equalToIgnoringCase(text));
     }
 }
--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -9,6 +9,9 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class MainPage extends LoadableComponent<MainPage> {
     private static final SelenideElement MAIN_NAV = $x("//div[@id='hook_Block_SideNavigation']");
+    private static final SelenideElement MSG_COUNT = $x("//div[@id='counter_ToolbarMessages']/div");
+
+    private static final SelenideElement IDEAS = $x("//*[@class='portlet_h_name_t __multiline']");
 
     @Override
     protected void load() {
@@ -22,5 +25,13 @@ public class MainPage extends LoadableComponent<MainPage> {
 
     public boolean isAuthorized() {
         return $(MAIN_NAV).has(Condition.visible);
+    }
+
+    public int getMessageCount() {
+        return Integer.parseInt($(MSG_COUNT).text());
+    }
+
+    public String getIdeas(){
+        return $(IDEAS).text();
     }
 }
